1821cd2b4e85bc611ade429ba796bd23cbfa2c0b
Kevin <kevinantoniowiyonolauw@mail.ugm.ac.id>
2025-05-01 01:02:45 +0700

    tes

--- product/frontend/app/modules/login/page.tsx
+++ product/frontend/app/modules/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import LoginPage from '@/login/LoginPage';
+import LoginPage from '@/login/page';
 
 export default function Home() {
   return <LoginPage />;
